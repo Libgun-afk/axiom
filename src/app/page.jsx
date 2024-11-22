6406139f7322b3c7335f6f2d1a8362887a1e7c75
Bilguun <Bilguun040728@gmail.com>
2024-11-22 12:50:46 +0800

    a

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -103,7 +103,14 @@
 import React from "react";
 
 const page = () => {
-  return <div>page</div>;
+  return (
+    <div>
+      page
+      <div>
+        <button>asd</button>
+      </div>
+    </div>
+  );
 };
 
 export default page;
